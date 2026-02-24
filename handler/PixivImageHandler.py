0602af8033fda6d7c4e8f7211d49b60a0ce29faf
psilabs-dev <113860476+psilabs-dev@users.noreply.github.com>
2026-02-23 21:07:28 -0800

    Initial downstream server commit
    - auto create database parent if not exist
    - update default config items for server
    - add pixiv server version to PixivConstant
    - enforce image download safety
    - save create and upload date info

--- handler/PixivImageHandler.py
+++ handler/PixivImageHandler.py
@@ -652,6 +652,12 @@ def process_image(caller,
                 db.insertStats(image.imageId, image.jd_rtv, image.jd_rtc,
                                image.bookmark_count, image.comment_count, image.image_response_count)
 
+            # Save date info
+            created_epoch = image.get_created_date_epoch()
+            uploaded_epoch = image.get_uploaded_date_epoch()
+            if created_epoch is not None or uploaded_epoch is not None:
+                db.insertDateInfo(image.imageId, created_epoch, uploaded_epoch)
+
             if len(manga_files) > 0:
                 if archive_mode_update_manga_image_paths:
                     # Rewrite manga save names to point to their place in the archive.
